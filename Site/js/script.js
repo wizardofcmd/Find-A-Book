65661dee8a72a0c0248abaad67ddeb2230552287
StephenMoran2000 <60134471+StephenMoran2000@users.noreply.github.com>
2020-10-28 14:25:39 +0000

    Feeling Lucky
    Atm uses a search function to look for title and author of book.

--- Site/js/script.js
+++ Site/js/script.js
@@ -1,31 +1,21 @@
-$(document).ready(function(){	
-
-   $("#myform").submit(function(){
-
-   	  var search = $("#books").val();
-   	 for{		
-   	  var url = "";
-   	  var img = "";
-      var title = "";
-      var author = "";
-
-   	  $.get("https://www.googleapis.com/books/v1/volumes?q=" + search,function(response){
-
-          for(i=0;i<response.items.length;i++)
-          {
-           title=$('<h5 class="center-align white-text">' + response.items[i].volumeInfo.title + '</h5>');  
-           author=$('<h5 class="center-align white-text"> By:' + response.items[i].volumeInfo.authors + '</h5>');
-           img = $('<img class="aligning card z-depth-5" id="dynamic"><br><a href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="btn red aligning">Read More</button></a>'); 	
-           url= response.items[i].volumeInfo.imageLinks.thumbnail;
-           img.attr('src', url);
-           title.appendTo('#result');
-           author.appendTo('#result');
-           img.appendTo('#result');
-          }
-   	  });
-      
-      }
-      return false;
-   });
-
-});
+function bookSearch(){
+	var search =document.getElementById('search').value
+	document.getElementById('results').innerHTML=""
+	console.log(search)
+	
+	$.ajax({
+		url:"https://www.googleapis.com/books/v1/volumes?q="+search,
+		dataType: "json",
+		
+		success:function(data){
+			for(i=0;i<data.items.length;i++){
+				results.innerHTML +="<h2>"+ data.items[i].volumeInfo.title+ "<h2>"
+				results.innerHTML +="<h2>"+ data.items[i].volumeInfo.author+ "<h2>"
+				img = $('<img class="aligning card z-depth-5" id="dynamic"><br><a href=' + response.items[i].volumeInfo.infoLink + '><button id="imagebutton" class="btn red aligning">Read More</button></a>'); 	
+				url= response.items[i].volumeInfo.imageLinks.thumbnail;
+			}
+		},
+		type:'GET'
+	});
+}
+document.getElementById('button').addEventListener('click',bookSearch, false)
