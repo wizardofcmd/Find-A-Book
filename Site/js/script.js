c5ae40df62ec2080c4e8e82b352b8b42d731fe71
StephenMoran2000 <60134471+StephenMoran2000@users.noreply.github.com>
2020-11-03 15:47:36 +0000

    This might be too dificult

--- Site/js/script.js
+++ Site/js/script.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){	
 
-   $("#myform").submit(function(){
-
-   	  var search = $("#books").val();
-   			
+		
+  $("#myform").submit(function(){
+   	  var search = randomLetter;	
    	  var url = "";
    	  var img = "";
       var title = "";
       var author = "";
-
-   	  $.get("https://www.googleapis.com/books/v1/volumes?q=" + search,function(response){
-		//have random letter be inputed into searchbar 
+		$.get("https://www.googleapis.com/books/v1/volumes?q=" + search,function(response){
+		
+   	  
           for(i=0;i<response.items.length;i++)
           {
            title=$('<h5 class="center-align white-text">' + response.items[i].volumeInfo.title + '</h5>');  
@@ -23,6 +22,13 @@ $(document).ready(function(){
            img.appendTo('#result');
           }
    	  });
-   });
+ 
+    });
 
-});
+ 
+//have random letter be inputed into searchbar 
+			function randomLetter() {
+				const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+				return alphabet[Math.floor(Math.random() * alphabet.length)]
+			}
+		});
