0fa0dc9e1b3d4197abead4840530ac736f9cbce8
naisuuu <cjmosawan@yahoo.ie>
2020-11-12 20:10:03 +0000

    succesful json parsing
    able to now pick which part of the json data we want to present

--- Site/php/bookhandler.php
+++ Site/php/bookhandler.php
@@ -1,10 +1,14 @@
 <?php
 	
 	$selected_val = $_POST['bookdata'];
-	
+	$results = array("");
 	$link = "https://www.googleapis.com/books/v1/volumes?q=subject:".$selected_val;
 	$book_json = file_get_contents($link);
-	//$data = json_decode($book_json,true);
-	
-	echo $book_json;
+	$data = json_decode($book_json,true);
+	for ($i=0;$i<count($data["items"]);$i++) {
+		$results[$i] = $data["items"][$i]["volumeInfo"];
+		
+	}
+	//echo $book_json;
+	echo json_encode($results);
 ?>
