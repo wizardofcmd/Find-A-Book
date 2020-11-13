374f8d9e954beeeacc0bd68b57b5f61edf1f3755
naisuuu <cjmosawan@yahoo.ie>
2020-11-13 01:26:27 +0000

    arbor initialized

--- Site/index.php
+++ Site/index.php
@@ -9,18 +9,21 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 		
-		<script src="js/apiscript.js"></script>
+		<!-- Arbor.js  -->
+		<script src="./scripts/jquery-1.6.1.min.js"></script>
+		<script src="./scripts/arbor.js"></script>
+		<script src="./scripts/graphics.js"></script>
+		<script src="./scripts/renderer.js"></script>
 		<script> 
 			
 			$(document).ready(function() {
 				$("#sl_books").hide(); 
+				$("#viewport").hide();
 				
-				
-				$( "#btn_fic_id" ).click(function() {
-					var book_data = "fiction";
-					alert(book_data);
-					console.log(book_data);
-					
+				$( "#btn_fic_id" ).click(function() { //if button clicked, do something
+					var book_data = "fiction"; //creates a new variable and saves it as a hardcoded string
+					alert(book_data + " button was clicked"); // for testing purposes
+					console.log(book_data);					  // for testing purposes
 					
 					$.ajax({type: "POST",		
 						url: './php/bookhandler.php',
@@ -29,11 +32,13 @@
 						
 						success: function(data){
 							$("#sl_books").show();
+							$("#viewport").show();
 							console.log(data);
-							var select = document.getElementById('sl_books'); // new var select
+							
+							var select = document.getElementById('opt_books'); // create javascript variable based on existing html tag
 							$(select).html('');
-							for (var i in data) { // move through array of data which you can get from test.php
-								$(select).append('<option value=' + data[i] + '>' + data[i]['title'] + '</option>'); // for every index create HTML as follows and append to #routes							
+							for (var i in data) { // move through data sent from bookhandler.php
+								$(select).append('<option value=' + data[i] + '>' + data[i]['title'] + '</option>'); // for every index create HTML as follows and append to chosen html tag							
 							}
 							
 						}});
@@ -56,10 +61,9 @@
 					}}); */
 				});
 				$( "#btn_nonfic_id" ).click(function() {
-					var book_data = "Nonfiction";
-					alert(book_data);
+					var book_data = "nonfiction";
+					alert(book_data + " button was clicked");
 					console.log(book_data);
-					
 					
 					$.ajax({type: "POST",
 						url: './php/bookhandler.php',
@@ -68,8 +72,10 @@
 						
 						success: function(data){
 							$("#sl_books").show();
+							$("#viewport").show();
 							console.log(data);
-							var select = document.getElementById('sl_books'); // new var select
+							
+							var select = document.getElementById('opt_books'); // new var select
 							$(select).html('');
 							for (var i in data) { // move through array of data which you can get from test.php
 								$(select).append('<option value=' + data[i] + '>' + data[i]['title'] + '</option>'); // for every index create HTML as follows and append to #routes							
@@ -78,10 +84,7 @@
 				});
 				
 				/*
-					
-					Need to be optimised
-					Switch statement?
-					
+					TO BE OPTIMIZED
 					if(document.getElementById('btn_fic_id').clicked == true)
 					{
 					var book_data = "fiction";
@@ -98,51 +101,85 @@
 				
 				
 				
-				});
-				</script>
-		</head>
-		<body>
-			<nav class="navbar navbar-inverse">
-				<div class="container">
-					<div class="navbar-header">
-						<a class="navbar-brand" href="#">FindABook</a>
-					</div>
-					<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse"> 
-						Menu
-					</button>
-					<div class="collapse navbar-collapse navHeaderCollapse">
-						<ul class ="nav navbar-nav navbar-right">
-							<li><a href="index.html"><span class="glyphicon glyphicon-home"></span>Home</a></li>
-							<li><a href="About/About.html"><span class="glyphicon glyphicon-info-sign"></span>About</a></li>
-							<li><a href="Review/review.html"><span class="glyphicon glyphicon-envelope"></span>Reviews</a></li>
-						</ul>
-					</div>
-				</nav>
-				<div class="container ">
-					
-					<div class="row btn-group btn-group-justified">
-						<div class="col-md-4">
-							<div class="container">		
-								<button value ="btn_fic_val" class="btn btn-info-dark" id="btn_fic_id"><img src="Images/Fiction.jpg" class="img-responsive" alt="Fiction">Fiction</button>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="container">	
-								<button value ="btn_ran_val " class="btn btn-info-dark" id="btn_ran_id"><img src="Images/Feeling Lucky.png" class="img-responsive" alt="Feeling Lucky">Feeling Lucky</button>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="container">
-								<button value="btn_nonfic_val"  class="btn btn-info-dark " id="btn_nonfic_id" ><img src="Images/Non-Fiction.jpg" class="img-responsive" alt="Non-Fiction">Non-Fiction</button>
-							</div>
-						</div>
-					</div>
-					
+			});
+		</script>
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">FindABook</a>
 				</div>
-				<select id="sl_books" name="sl_books"> 
-					<optgroup id="opt_books" label="opt_books">
-						
-					</optgroup>
-				</select>
-			</body>
-		</html>																
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse"> 
+					Menu
+				</button>
+				<div class="collapse navbar-collapse navHeaderCollapse">
+					<ul class ="nav navbar-nav navbar-right">
+						<li><a href="index.html"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+						<li><a href="About/About.html"><span class="glyphicon glyphicon-info-sign"></span>About</a></li>
+						<li><a href="Review/review.html"><span class="glyphicon glyphicon-envelope"></span>Reviews</a></li>
+					</ul>
+				</div>
+			</nav>
+			<div class="container ">
+				
+				<div class="row btn-group btn-group-justified">
+					<div class="col-md-4">
+						<div class="container">		
+							<button value ="btn_fic_val" class="btn btn-info-dark" id="btn_fic_id"><img src="Images/Fiction.jpg" class="img-responsive" alt="Fiction">Fiction</button>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="container">	
+							<button value ="btn_ran_val " class="btn btn-info-dark" id="btn_ran_id"><img src="Images/Feeling Lucky.png" class="img-responsive" alt="Feeling Lucky">Feeling Lucky</button>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="container">
+							<button value="btn_nonfic_val"  class="btn btn-info-dark " id="btn_nonfic_id" ><img src="Images/Non-Fiction.jpg" class="img-responsive" alt="Non-Fiction">Non-Fiction</button>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+			<select id="sl_books" name="sl_books"> 
+				<optgroup id="opt_books" label="test_book_titles"></optgroup>
+			</select>
+			<!-- Arbor.js -->
+				<canvas id="viewport" width="800" height="600"></canvas>
+				<script language="javascript" type="text/javascript">
+					var sys = arbor.ParticleSystem(1000, 400,1);
+					sys.parameters({gravity:true});
+					sys.renderer = Renderer("#viewport");
+					
+					var data = {
+						nodes:{
+							animals:{'color':'red','shape':'dot','label':'This'},
+							dog:{'color':'green','shape':'dot','label':'is'},
+							cat:{'color':'blue','shape':'dot','label':'for'},
+							toy:{'color':'pink','shape':'dot','label':'testing'}
+						},
+						edges:{
+							animals:{ dog:{'color':'green'}, cat:{},toy:{}},
+							dog:{ cat:{}}
+						}
+					};
+					sys.graft(data);
+					
+					setTimeout(function(){
+						var postLoadData = {
+							nodes:{
+								joe:{'color':'orange','shape':'dot','label':'purposes'},
+								fido:{'color':'green','shape':'dot','label':'only'},
+								fluffy:{'color':'blue','shape':'dot','label':':D'}
+							},
+							edges:{
+								dog:{ fido:{} },
+								cat:{ fluffy:{} },
+								joe:{ fluffy:{},fido:{} }
+							}
+						};
+					sys.graft(postLoadData);});
+				</script>
+		</body>
+	</html>																		
--- Site/js/apiscript.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	
-	
-	return false;
-	
-});
