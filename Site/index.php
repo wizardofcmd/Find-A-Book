d0451b615a4e4373c67f2d703c9b9f093a332a2d
naisuuu <cjmosawan@yahoo.ie>
2020-11-13 00:06:11 +0000

    printable, retrieved book titles

--- Site/index.php
+++ Site/index.php
@@ -13,82 +13,113 @@
 		<script> 
 			
 			$(document).ready(function() {
-			$( "#btn_fic_id" ).click(function() {
-			//alert("asd");
-			var book_data = "fiction";
-			console.log(book_data);
-			
-			
-			$.ajax({type: "POST",		
-			url: './php/bookhandler.php',
-			data: {bookdata: book_data},
-			dataType: 'json',
-			
-			success: function(data){
-			console.log(data);
-			
-			}});
-			});
-			$( "#btn_ran_id" ).click(function() {
-			
-			var book_data = "please index.php line 34"; // Needs a randomizer that saves 'book_data' as a random string 
-			alert(book_data);
-			console.log(book_data);
-			
-			
-			/*$.ajax({type: "POST",
-			url: './php/bookhandler.php',
-			data: {bookdata: book_data},  
-			dataType: 'json',
-			
-			success: function(data){
-			console.log(data);
-			
-			}}); */
-			});
-			$( "#btn_nonfic_id" ).click(function() {
-			//alert("asd");
-			var book_data = "Nonfiction";
-			alert(book_data);
-			console.log(book_data);
-			
-			
-			$.ajax({type: "POST",
-			url: './php/bookhandler.php',
-			data: {bookdata: book_data},
-			dataType: 'json',
-			
-			success: function(data){
-			console.log(data);
-			
-			}});
-			});
-			
-			
-			
-			
-			});
-		</script>
-	</head>
-	<body>
-		<nav class="navbar navbar-inverse">
-			<div class="container">
-				<div class="navbar-header">
-					<a class="navbar-brand" href="#">FindABook</a>
-				</div>
-				<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse"> 
-					Menu
-				</button>
-				<div class="collapse navbar-collapse navHeaderCollapse">
-					<ul class ="nav navbar-nav navbar-right">
-						<li><a href="index.html"><span class="glyphicon glyphicon-home"></span>Home</a></li>
-						<li><a href="About/About.html"><span class="glyphicon glyphicon-info-sign"></span>About</a></li>
-						<li><a href="Review/review.html"><span class="glyphicon glyphicon-envelope"></span>Reviews</a></li>
-					</ul>
-				</div>
-			</nav>
-			<div class="container ">
+				$("#sl_books").hide(); 
 				
+				
+				$( "#btn_fic_id" ).click(function() {
+					var book_data = "fiction";
+					alert(book_data);
+					console.log(book_data);
+					
+					
+					$.ajax({type: "POST",		
+						url: './php/bookhandler.php',
+						data: {bookdata: book_data},
+						dataType: 'json',
+						
+						success: function(data){
+							$("#sl_books").show();
+							console.log(data);
+							var select = document.getElementById('sl_books'); // new var select
+							$(select).html('');
+							for (var i in data) { // move through array of data which you can get from test.php
+								$(select).append('<option value=' + data[i] + '>' + data[i]['title'] + '</option>'); // for every index create HTML as follows and append to #routes							
+							}
+							
+						}});
+				});
+				$( "#btn_ran_id" ).click(function() {
+					
+					var book_data = "please read index.php line 34"; // Needs a randomizer that saves 'book_data' as a random string 
+					alert(book_data);
+					console.log(book_data);
+					
+					
+					/*$.ajax({type: "POST",
+						url: './php/bookhandler.php',
+						data: {bookdata: book_data},  
+						dataType: 'json',
+						
+						success: function(data){
+						console.log(data);
+						
+					}}); */
+				});
+				$( "#btn_nonfic_id" ).click(function() {
+					var book_data = "Nonfiction";
+					alert(book_data);
+					console.log(book_data);
+					
+					
+					$.ajax({type: "POST",
+						url: './php/bookhandler.php',
+						data: {bookdata: book_data},
+						dataType: 'json',
+						
+						success: function(data){
+							$("#sl_books").show();
+							console.log(data);
+							var select = document.getElementById('sl_books'); // new var select
+							$(select).html('');
+							for (var i in data) { // move through array of data which you can get from test.php
+								$(select).append('<option value=' + data[i] + '>' + data[i]['title'] + '</option>'); // for every index create HTML as follows and append to #routes							
+							}
+						}});
+				});
+				
+				/*
+					
+					Need to be optimised
+					Switch statement?
+					
+					if(document.getElementById('btn_fic_id').clicked == true)
+					{
+					var book_data = "fiction";
+					alert(" fiction button was clicked");
+					} else if (document.getElementById('btn_ran_id').clicked == true)
+					{
+					var book_data = "please read index.php line 34";
+					alert("button was clicked");
+					} else if(document.getElementById('btn_nonfic_id').clicked == true)
+					{
+					var book_data = "Nonfiction";
+					alert(" nonfic button was clicked");
+				}*/ 
+				
+				
+				
+				});
+				</script>
+		</head>
+		<body>
+			<nav class="navbar navbar-inverse">
+				<div class="container">
+					<div class="navbar-header">
+						<a class="navbar-brand" href="#">FindABook</a>
+					</div>
+					<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse"> 
+						Menu
+					</button>
+					<div class="collapse navbar-collapse navHeaderCollapse">
+						<ul class ="nav navbar-nav navbar-right">
+							<li><a href="index.html"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+							<li><a href="About/About.html"><span class="glyphicon glyphicon-info-sign"></span>About</a></li>
+							<li><a href="Review/review.html"><span class="glyphicon glyphicon-envelope"></span>Reviews</a></li>
+						</ul>
+					</div>
+				</nav>
+				<div class="container ">
+					
 					<div class="row btn-group btn-group-justified">
 						<div class="col-md-4">
 							<div class="container">		
@@ -106,8 +137,12 @@
 							</div>
 						</div>
 					</div>
-				
-			</div>
-			<select id="bookselect">Books</select>
-		</body>
-	</html>									
+					
+				</div>
+				<select id="sl_books" name="sl_books"> 
+					<optgroup id="opt_books" label="opt_books">
+						
+					</optgroup>
+				</select>
+			</body>
+		</html>																
--- Site/php/bookhandler.php
+++ Site/php/bookhandler.php
@@ -1,14 +1,15 @@
 <?php
 	
 	$selected_val = $_POST['bookdata'];
-	$results = array("");
+	$titles = array("");
 	$link = "https://www.googleapis.com/books/v1/volumes?q=subject:".$selected_val;
 	$book_json = file_get_contents($link);
 	$data = json_decode($book_json,true);
+	
 	for ($i=0;$i<count($data["items"]);$i++) {
-		$results[$i] = $data["items"][$i]["volumeInfo"];
+		$titles[$i] = $data["items"][$i]["volumeInfo"];
 		
 	}
 	//echo $book_json;
-	echo json_encode($results);
+	echo json_encode($titles);
 ?>
