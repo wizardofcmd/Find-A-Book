22ff613fe270de6bbce04d58d30f74466bfb6a79
StephenMoran2000 <60134471+StephenMoran2000@users.noreply.github.com>
2020-11-17 09:17:40 +0000

    Revert "Merge branch 'master' of https://github.com/wizardofcmd/Find-A-Book"
    This reverts commit 7559b5f61677c42b4a7ecf7f480dd80351dbdd8c, reversing
    changes made to 147a1e72ad1e0a09817aff9c2df70d67f3f18ba3.

--- Site/index.php
+++ Site/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
-"http://www.w3.org/TR/html4/strict.dtd">
+   "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 	<head>
 		<title>FindABook Home</title>
@@ -16,16 +16,20 @@
 		<script src="./scripts/graphics.js"></script>
 		<script src="./scripts/renderer.js"></script>
 		<script> 
+			
 			$(document).ready(function(){
 				$("#sl_books").hide(); 
 				$("#viewport").hide();
+				var bk_desc[i] = "";
+				var bk_author;
+				var bk_categ;
+				var bk_img_link;
+				var bk_inf_link;
+				var bk_prvw_link;
+				var bk_rating;
+				var bk_pg_count;
 				
-				var items;
-				var title_arr; // where array of titles are going to be saved from data
-				var auth_arr;
-				var categ_arr;
-				var img_link_arr;
-				var info_link_arr;
+
 				$( "#btn_fic_id" ).click(function() { //if button clicked, do something
 					
 					
@@ -33,7 +37,7 @@
 					alert(book_data + " button was clicked"); // for testing purposes
 					console.log(book_data);					  // for testing purposes
 					
-					$.ajax({type: "POST",
+					$.ajax({type: "POST",		
 						url: './php/bookhandler.php',
 						data: {bookdata: book_data},
 						dataType: 'json',
@@ -41,81 +45,20 @@
 						success: function(data){
 							$("#sl_books").show();
 							$("#viewport").show();
-							//console.log(data);
 							
 							
-							
-							// for # of books retrieved, create long string of all titles/authors/categories/etc.
-							for (var i = 0; i < data.length - 1 ; i++) {
-								title_arr += data[i].title+"||"; //gets data from json for every variable
-								auth_arr += JSON.stringify(data[i].authors)+"||"; // have to stringify because its an object
-								categ_arr += JSON.stringify(data[i].categories)+"||";
-								img_link_arr += data[i].imageLinks.thumbnail+"||";
-								info_link_arr += data[i].infoLink+"||"; 
-							}
+							console.log(data);
 							
 							var select = document.getElementById('opt_books'); // create javascript variable based on existing html tag
 							$(select).html('');
 							for (var i in data) { // move through data sent from bookhandler.php
-								
-								$(select).append('<option value=' + data[i] + '>' + data[i]['title'] + '</option>'); // for every index create HTML as follows and append to chosen html tag							
-								
-								
+								$(select).append('<option value=' + data[i] + '>' + data[i]['title'] + '</option>'); // for every index create HTML as follows and append to chosen html tag
+								bk_desc[i] = data[i]['description'];
 							}
 							
-							// list of arrays
-							var titles = title_arr.split('||');
-							var titles_undef = titles[0]; // to remove 'undefined' as part of string
-							titles_undef = titles_undef.substring(9); // removes 9 characters from the start: u n d e f i n e d
-							titles[0] = titles_undef; // replaces first index
-							
-							var authors = auth_arr.split('||'); // split long string into array elements
-							var authors_undef = authors[0];
-							authors_undef = authors_undef.substring(9);
-							authors[0] = authors_undef;
-							
-							var categories = categ_arr.split('||');
-							var categories_undef = categories[0];
-							categories_undef = categories_undef.substring(9);
-							categories[0] = categories_undef;
-							
-							var image_links = img_link_arr.split('||');
-							var image_links_undef = image_links[0];
-							image_links_undef = image_links_undef.substring(9);
-							image_links[0] = image_links_undef;
-							
-							var info_links = info_link_arr.split('||');
-							var info_links_undef = info_links[0];
-							info_links_undef = info_links_undef.substring(9);
-							info_links[0] = info_links_undef;
-							
-							// creates book objects aka maps using title as the 'primary key'
-							
-							
-							items = titles.map((title, i) => {
-								return {
-									title: title,
-									author: authors[i],
-									category: categories[i],
-									imageLinks: image_links[i],
-									infoLink: info_links[i]
-									
-								}
-							});
-							
-							
-							/*console.log(titles);
-								console.log(authors);
-								console.log(categories);
-								console.log(image_links);
-							console.log(data);*/
-							
-							console.log(items);
-							document.getElementById("demo").innerHTML = titles;
 						}});
+						
 				});
-				
-				
 				
 				$( "#btn_nonfic_id" ).click(function() {
 					var book_data = "nonfiction";
@@ -137,55 +80,6 @@
 							for (var i in data) { // move through data sent from bookhandler.php
 								$(select).append('<option value=' + data[i] + '>' + data[i]['title'] + '</option>'); // for every index create HTML as follows and append to chosen html tag							
 							}
-							
-							
-							// list of arrays
-							var titles = title_arr.split('||');
-							var titles_undef = titles[0]; // to remove 'undefined' as part of string
-							titles_undef = titles_undef.substring(9); // removes 9 characters from the start: u n d e f i n e d
-							titles[0] = titles_undef; // replaces first index
-							
-							var authors = auth_arr.split('||'); // split long string into array elements
-							var authors_undef = authors[0];
-							authors_undef = authors_undef.substring(9);
-							authors[0] = authors_undef;
-							
-							var categories = categ_arr.split('||');
-							var categories_undef = categories[0];
-							categories_undef = categories_undef.substring(9);
-							categories[0] = categories_undef;
-							
-							var image_links = img_link_arr.split('||');
-							var image_links_undef = image_links[0];
-							image_links_undef = image_links_undef.substring(9);
-							image_links[0] = image_links_undef;
-							
-							var info_links = info_link_arr.split('||');
-							var info_links_undef = info_links[0];
-							info_links_undef = info_links_undef.substring(9);
-							info_links[0] = info_links_undef;
-							
-							// creates book objects aka maps using title as the 'primary key'
-							items = titles.map((title, i) => {
-								return {
-									title: title,
-									author: authors[i],
-									category: categories[i],
-									imageLinks: image_links[i],
-									infoLink: info_links[i]
-								}
-							});
-							
-							
-							/*console.log(titles);
-								console.log(authors);
-								console.log(categories);
-								console.log(image_links);
-							console.log(data);*/
-							
-							console.log(items);
-							document.getElementById("demo").innerHTML = titles;
-							
 						}});
 				});
 				
@@ -205,47 +99,9 @@
 					alert(" nonfic button was clicked");
 				}*/ 
 				
-				var sys = arbor.ParticleSystem(1000, 400,1);
-				sys.parameters({gravity:true});
-				sys.renderer = Renderer("#viewport");
 				
 				
-				var data = {
-					
-					
-					nodes:{
-						/*for (var i = 0; i < items.length - 1 ; i++) {
-							items[i]['title']:{'color':'red','shape':'dot'}
-							
-						}*/
-						
-					},
-					edges:{
-						animals:{ dog:{'color':'green'}, cat:{},toy:{}},
-						dog:{ cat:{}}
-					}
-				};
-				
-				sys.graft(data);
-				
-				setTimeout(function(){
-					var postLoadData = {
-						nodes:{
-							joe:{'color':'orange','shape':'dot','label':'purposes'},
-							fido:{'color':'green','shape':'dot','label':'only'},
-							fluffy:{'color':'blue','shape':'dot','label':':D'}
-						},
-						edges:{
-							testic:{ fido:{} },
-							cat:{ fluffy:{} },
-							joe:{ fluffy:{},fido:{}}
-						}
-					};
-					sys.graft(postLoadData);
-				});
 			});
-			
-			
 		</script>
 	</head>
 	<body>
@@ -275,11 +131,9 @@
 					</div>
 					<div class="col-md-4">
 						<div class="container">	
-							
-							<a href="FeelingLucky/FeelingLucky.html">
-								<button value ="btn_ran_val " class="btn btn-info-dark" id="btn_ran_id"><img src="Images/Feeling Lucky.png" class="img-responsive" alt="Feeling Lucky">Feeling Lucky</button>
-							</a>
-							
+						<a href="FeelingLucky/FeelingLucky.html">
+							<button value ="btn_ran_val " class="btn btn-info-dark" id="btn_ran_id"><img src="Images/Feeling Lucky.png" class="img-responsive" alt="Feeling Lucky">Feeling Lucky</button>
+						</a>
 						</div>
 					</div>
 					<div class="col-md-4">
@@ -293,15 +147,51 @@
 			<select id="sl_books" name="sl_books"> 
 				<optgroup id="opt_books" label="test_book_titles"></optgroup>
 			</select>
-			
-			
-			
-			<p id="demo"></p>
-			
 			<!-- Arbor.js -->
-			<canvas id="viewport" width="800" height="600"></canvas>
 			
+			
+				<canvas id="viewport" width="800" height="600"></canvas>
+				<script language="javascript" type="text/javascript">
+					var sys = arbor.ParticleSystem(1000, 400,1);
+					sys.parameters({gravity:true});
+					sys.renderer = Renderer("#viewport");
+					
+					
+					var data = {
+						nodes:{
+							animals:{'color':'red','shape':'dot','label':'This'},
+							dog:{'color':'green','shape':'dot','label':'is'},
+							cat:{'color':'blue','shape':'dot','label':'for'},
+							toy:{'color':'pink','shape':'dot','label':'testing'}
+						},
+						edges:{
+							animals:{ dog:{'color':'green'}, cat:{},toy:{}},
+							dog:{ cat:{}}
+						}
+					};
+					sys.graft(data);
+					var testic= 'TEST';
+					setTimeout(function(){
+						var postLoadData = {
+							nodes:{
+								joe:{'color':'orange','shape':'dot','label':'purposes'},
+								fido:{'color':'green','shape':'dot','label':'only'},
+								fluffy:{'color':'blue','shape':'dot','label':':D'}
+							},
+							edges:{
+								testic:{ fido:{} },
+								cat:{ fluffy:{} },
+								joe:{ fluffy:{},fido:{}}
+							}
+						};
+					sys.graft(postLoadData);});
+				</script>
+				<div class="alt">
+					<div class="container fixed-bottom">	
+						<footer class="page-footer font-small  pt-4 special-color-dark">
+							<div class="footer-copyright text-center py-3"></br>&copy;  FindABook<br/></div>
+						</footer>
+					</div>
+				</div>
 		</body>
-	</html>																																							
-	
-	
+	</html>																		
