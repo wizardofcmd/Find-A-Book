8890b4da79cd9f77bfaa1f3f97c9fd988d8495bf
LingRob <35962609+LingRob@users.noreply.github.com>
2020-11-26 18:52:23 +0000

    Update index.php

--- Site/webpages/index.php
+++ Site/webpages/index.php
@@ -15,7 +15,10 @@
 		<script src="../js/renderer.js"></script>
 		
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico" />
+
+        <link rel="icon" type="image/x-icon" href="./assets/img/portfolio/circle-cropped.png" />
+
+
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -54,7 +57,9 @@
         <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
                 <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="../assets/img/avataaars.svg" alt="" />
+
+                <img class="masthead-avatar mb-5" src="../assets/img/portfolio/fabi.png" alt="" />
+
                 <!-- Masthead Heading-->
                 <h1 class="masthead-heading text-uppercase mb-0">FindYourBook</h1>
                 <!-- Icon Divider-->
@@ -64,7 +69,7 @@
                     <div class="divider-custom-line"></div>
                 </div>
                 <!-- Masthead Subheading-->
-                <p class="masthead-subheading font-weight-light mb-0">Cyril - Roberto - Sorin - Sthephen - Filip</p>
+                <p class="masthead-subheading font-weight-light mb-0">The hardest part is picking the right one, not anymore !</p>
             </div>
         </header>
         <!-- Portfolio Section-->
@@ -173,54 +178,30 @@
 		
 		
         <!-- Contact Section-->
-        <section class="page-section" id="contact">
+        <!-- About Section-->
+        <section class="page-section bg-primary text-white mb-0" id="contact">
             <div class="container">
-                <!-- Contact Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
                 <!-- Icon Divider-->
-                <div class="divider-custom">
+                <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
                     <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                     <div class="divider-custom-line"></div>
                 </div>
-                <!-- Contact Section Form-->
+                <!-- About Section Content-->
                 <div class="row">
-                    <div class="col-lg-8 mx-auto">
-                        <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.-->
-                        <form id="contactForm" name="sentMessage" novalidate="novalidate">
-                            <div class="control-group">
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Name</label>
-                                    <input class="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name." />
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div class="control-group">
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Email Address</label>
-                                    <input class="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address." />
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div class="control-group">
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Phone Number</label>
-                                    <input class="form-control" id="phone" type="tel" placeholder="Phone Number" required="required" data-validation-required-message="Please enter your phone number." />
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div class="control-group">
-                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                    <label>Message</label>
-                                    <textarea class="form-control" id="message" rows="5" placeholder="Message" required="required" data-validation-required-message="Please enter a message."></textarea>
-                                    <p class="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <br />
-                            <div id="success"></div>
-                            <div class="form-group"><button class="btn btn-primary btn-xl" id="sendMessageButton" type="submit">Send</button></div>
-                        </form>
-                    </div>
+                    <div class="col-lg-4 ml-auto"><p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
+                    <div class="col-lg-4 mr-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+                    <div class="col-lg-4 mr-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+
+                </div>
+                <!-- About Section Button-->
+                <div class="text-center mt-4">
+                    <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/theme/freelancer/">
+                        <i class="fas fa-download mr-2"></i>
+                        Free Download!
+                    </a>
                 </div>
             </div>
         </section>
@@ -285,10 +266,64 @@
                                         <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                                         <div class="divider-custom-line"></div>
                                     </div>
+<<<<<<< HEAD
+                                    <!-- Portfolio Modal - The things you want to display/hide go here-->
+                                    <div class="container ">
+				<div class="row btn-group btn-group-justified">
+					<div class="col-md-4">
+						<div class="container">		
+							<button value ="btn_fic_val" class="btn btn-info-dark" id="btn_fic_id"><img src="Images/Fiction.jpg" class="img-responsive" alt="Fiction">Fiction</button>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="container">	
+							<button value ="btn_ran_val " class="btn btn-info-dark" id="btn_ran_id"><img src="Images/Feeling Lucky.png" class="img-responsive" alt="Feeling Lucky">Feeling Lucky</button>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="container">
+							<button value="btn_nonfic_val"  class="btn btn-info-dark " id="btn_nonfic_id" ><img src="Images/Non-Fiction.jpg" class="img-responsive" alt="Non-Fiction">Non-Fiction</button>
+						</div>
+					</div>
+				</div>		
+            </div>
+            
+                                        <!-- Icon Divider-->
+                                       <div class="divider-custom">
+                                        <div class="divider-custom-line"></div>
+                                        
+                                        <div class="divider-custom-line"></div>
+                                       </div>
+								
+			<div class="container ">
+				<div class="row btn-group btn-group-justified">
+					<div class="col-md-4">
+						<div class="container">		
+							<button value ="btn_fic_val" class="btn btn-info-dark" id="btn_fic_id"><img src="Images/Fiction.jpg" class="img-responsive" alt="Fiction">Fiction</button>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="container">	
+							<button value ="btn_ran_val " class="btn btn-info-dark" id="btn_ran_id"><img src="Images/Feeling Lucky.png" class="img-responsive" alt="Feeling Lucky">Feeling Lucky</button>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="container">
+							<button value="btn_nonfic_val"  class="btn btn-info-dark " id="btn_nonfic_id" ><img src="Images/Non-Fiction.jpg" class="img-responsive" alt="Non-Fiction">Non-Fiction</button>
+						</div>
+					</div>
+				</div>		
+			</div>
+                                    <div class="divider-custom">
+                                    </div>			
+=======
                                     <!-- Portfolio Modal - Image-->
                                     <img class="img-fluid rounded mb-5" src="../assets/img/portfolio/cabin.png" alt="" />
+<<<<<<< Updated upstream
+=======
+>>>>>>> 8730104f5028412a917436e829a7a8d58e35bcff
+>>>>>>> Stashed changes
                                     <!-- Portfolio Modal - Text-->
-                                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
                                     <button class="btn btn-primary" data-dismiss="modal">
                                         <i class="fas fa-times fa-fw"></i>
                                         Close Window
@@ -319,7 +354,7 @@
                                         <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                                         <div class="divider-custom-line"></div>
                                     </div>
-                                    <!-- Portfolio Modal - Image-->
+                                    <!-- Portfolio Modal - The things you want to display/hide go here-->
             <div class="container ">
 				<div class="row btn-group btn-group-justified">
 					<div class="col-md-4">
@@ -399,10 +434,66 @@
                                         <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                                         <div class="divider-custom-line"></div>
                                     </div>
+<<<<<<< HEAD
+
+
+                                    <!-- Portfolio Modal - The things you want to display/hide go here-->
+                                    <div class="container ">
+				<div class="row btn-group btn-group-justified">
+					<div class="col-md-4">
+						<div class="container">		
+							<button value ="btn_fic_val" class="btn btn-info-dark" id="btn_fic_id"><img src="Images/Fiction.jpg" class="img-responsive" alt="Fiction">Fiction</button>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="container">	
+							<button value ="btn_ran_val " class="btn btn-info-dark" id="btn_ran_id"><img src="Images/Feeling Lucky.png" class="img-responsive" alt="Feeling Lucky">Feeling Lucky</button>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="container">
+							<button value="btn_nonfic_val"  class="btn btn-info-dark " id="btn_nonfic_id" ><img src="Images/Non-Fiction.jpg" class="img-responsive" alt="Non-Fiction">Non-Fiction</button>
+						</div>
+					</div>
+				</div>		
+            </div>
+            
+                                        <!-- Icon Divider-->
+                                       <div class="divider-custom">
+                                        <div class="divider-custom-line"></div>
+                                        
+                                        <div class="divider-custom-line"></div>
+                                       </div>
+								
+			<div class="container ">
+				<div class="row btn-group btn-group-justified">
+					<div class="col-md-4">
+						<div class="container">		
+							<button value ="btn_fic_val" class="btn btn-info-dark" id="btn_fic_id"><img src="Images/Fiction.jpg" class="img-responsive" alt="Fiction">Fiction</button>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="container">	
+							<button value ="btn_ran_val " class="btn btn-info-dark" id="btn_ran_id"><img src="Images/Feeling Lucky.png" class="img-responsive" alt="Feeling Lucky">Feeling Lucky</button>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="container">
+							<button value="btn_nonfic_val"  class="btn btn-info-dark " id="btn_nonfic_id" ><img src="Images/Non-Fiction.jpg" class="img-responsive" alt="Non-Fiction">Non-Fiction</button>
+						</div>
+					</div>
+				</div>		
+			</div>
+                                    <div class="divider-custom">
+                                    </div>	
+=======
                                     <!-- Portfolio Modal - Image-->
                                     <img class="img-fluid rounded mb-5" src="../assets/img/portfolio/circus.png" alt="" />
+<<<<<<< Updated upstream
+=======
+>>>>>>> 8730104f5028412a917436e829a7a8d58e35bcff
+>>>>>>> Stashed changes
                                     <!-- Portfolio Modal - Text-->
-                                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
                                     <button class="btn btn-primary" data-dismiss="modal">
                                         <i class="fas fa-times fa-fw"></i>
                                         Close Window
