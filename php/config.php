bae03297e3a9c7861508a9c619f346b2da980eec
naisuuu <cjmosawan@yahoo.ie>
2020-12-02 11:42:00 +0000

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,7 +1,8 @@
 <?php
 	$host='zwgaqwfn759tj79r.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
 	$user='fbwp5u68u9cijt3z';
-	$pass'dbsauqmq7lts255i';
+	$pass='dbsauqmq7lts255i';
+	$db='dzlgt6jdd573rkdc';
 	
 	$mysqli = new mysqli($host, $user, $pass, '');
 	if ($mysqli->connect_error) {
