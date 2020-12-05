66b4c1c8ea1c2701a27e20a29c7f789fd6965e1d
naisuuu <cjmosawan@yahoo.ie>
2020-12-05 18:35:14 +0000

    functioning juvenile button

--- js/buttons.js
+++ js/buttons.js
@@ -1,3 +1,83 @@
+function ajax($this) {
+	var book_data = $this.attr('name');
+	console.log(book_data);
+	var sys = arbor.ParticleSystem(1000, 400,1);
+	sys.parameters({gravity:true});
+	sys.renderer = Renderer("#viewport");
+	
+	
+	var items;
+	var title_arr; // where array of titles are going to be saved from data
+	var auth_arr;
+	var categ_arr;
+	var img_link_arr;
+	var info_link_arr;
+    $.ajax({
+        type: "POST",
+        url: './php/bookhandler.php',
+        dataType: 'json',
+        data: {bookdata: book_data},
+        success: function (data) {
+            $("#viewport").show();
+			
+			for (var i = 0; i < data.length - 1 ; i++) {// for # of books retrieved, create long string of all titles/authors/categories/etc.
+				title_arr += data[i].title+"||"; //gets data from json for every variable
+				auth_arr += JSON.stringify(data[i].authors)+"||"; // have to stringify because its an object
+				categ_arr += JSON.stringify(data[i].categories)+"||";
+				img_link_arr += data[i].imageLinks.thumbnail+"||";
+				info_link_arr += data[i].infoLink+"||"; 
+			}
+			
+			// list of arrays
+			var titles = title_arr.split('||');var titles_undef = titles[0]; // to remove 'undefined' as part of string
+			titles_undef = titles_undef.substring(9); // removes 9 characters from the start: u n d e f i n e d
+			titles[0] = titles_undef; // replaces first index
+			var authors = auth_arr.split('||'); // split long string into array elements
+			var authors_undef = authors[0];
+			authors_undef = authors_undef.substring(9);
+			authors[0] = authors_undef;
+			var categories = categ_arr.split('||');
+			var categories_undef = categories[0];
+			categories_undef = categories_undef.substring(9);
+			categories[0] = categories_undef;
+			var image_links = img_link_arr.split('||');
+			var image_links_undef = image_links[0];
+			image_links_undef = image_links_undef.substring(9);
+			image_links[0] = image_links_undef;
+			var info_links = info_link_arr.split('||');
+			var info_links_undef = info_links[0];
+			info_links_undef = info_links_undef.substring(9);
+			info_links[0] = info_links_undef;
+			
+			
+			var b_data={nodes:{},}; // Declare variables with nested childs, ready to be grafted
+			var genre={
+				genre:{
+					label:'TEST',
+					color:'orange'
+				}
+			};
+			// Populate a variable with the book items
+			var nodes ={};
+			for (var i = 0; i < data.length -1; i++) {
+				nodes['book_item'+i]={};
+				nodes['book_item'+i].label=titles[i];
+				nodes['book_item'+i].author=authors[i];
+				nodes['book_item'+i].category=categories[i];
+				nodes['book_item'+i].image=image_links[i];
+				//nodes['book_item'+i].shape='dot';		
+			}
+			Object.assign(b_data.nodes,genre);
+			Object.assign(b_data.nodes,nodes);// Insert data from nodes into b_data.nodes
+			sys.graft(b_data);// Draw b_data and its data into canvas
+			console.log(b_data);// for testing
+			
+		},
+		fail: function(xhr, textStatus, errorThrown){
+			alert('request failed');
+		}
+	});
+}
 
 $(document).ready(function(){
 	$("#sl_books").hide(); 
@@ -11,17 +91,7 @@ $(document).ready(function(){
 	
 	
 	
-	var sys = arbor.ParticleSystem(1000, 400,1);
-	sys.parameters({gravity:true});
-	sys.renderer = Renderer("#viewport");
 	
-	
-	var items;
-	var title_arr; // where array of titles are going to be saved from data
-	var auth_arr;
-	var categ_arr;
-	var img_link_arr;
-	var info_link_arr;
 	
 	
 	/*$( "#btn_fic_id" ).click(function() { //if button clicked, do something
@@ -123,6 +193,7 @@ $(document).ready(function(){
 		
 		});
 	*/
+	
 	$( "#btn_nonfic_id" ).click(function() {
 		var book_data = "nonfiction";
 		//alert(book_data + " button was clicked");
@@ -134,14 +205,9 @@ $(document).ready(function(){
 			dataType: 'json',
 			
 			success: function(data){
-				$("#sl_books").show();
 				$("#viewport").show();
-				//console.log(data);
 				
-				
-				
-				// for # of books retrieved, create long string of all titles/authors/categories/etc.
-				for (var i = 0; i < data.length - 1 ; i++) {
+				for (var i = 0; i < data.length - 1 ; i++) {// for # of books retrieved, create long string of all titles/authors/categories/etc.
 					title_arr += data[i].title+"||"; //gets data from json for every variable
 					auth_arr += JSON.stringify(data[i].authors)+"||"; // have to stringify because its an object
 					categ_arr += JSON.stringify(data[i].categories)+"||";
@@ -149,50 +215,35 @@ $(document).ready(function(){
 					info_link_arr += data[i].infoLink+"||"; 
 				}
 				
-				
 				// list of arrays
-				var titles = title_arr.split('||');
-				var titles_undef = titles[0]; // to remove 'undefined' as part of string
+				var titles = title_arr.split('||');var titles_undef = titles[0]; // to remove 'undefined' as part of string
 				titles_undef = titles_undef.substring(9); // removes 9 characters from the start: u n d e f i n e d
 				titles[0] = titles_undef; // replaces first index
-				
 				var authors = auth_arr.split('||'); // split long string into array elements
 				var authors_undef = authors[0];
 				authors_undef = authors_undef.substring(9);
 				authors[0] = authors_undef;
-				
 				var categories = categ_arr.split('||');
 				var categories_undef = categories[0];
 				categories_undef = categories_undef.substring(9);
 				categories[0] = categories_undef;
-				
 				var image_links = img_link_arr.split('||');
 				var image_links_undef = image_links[0];
 				image_links_undef = image_links_undef.substring(9);
 				image_links[0] = image_links_undef;
-				
 				var info_links = info_link_arr.split('||');
 				var info_links_undef = info_links[0];
 				info_links_undef = info_links_undef.substring(9);
 				info_links[0] = info_links_undef;
 				
 				
-				// Declare variables with nested childs, ready to be grafted
-				
-				var b_data={
-					nodes:{},
-					
-					
-				};
+				var b_data={nodes:{},}; // Declare variables with nested childs, ready to be grafted
 				var genre={
 					genre:{
-						label:'Non-Fiction',
+						label:'TEST',
 						color:'orange'
 					}
 				};
-				
-				
-				
 				// Populate a variable with the book items
 				var nodes ={};
 				for (var i = 0; i < data.length -1; i++) {
@@ -201,29 +252,12 @@ $(document).ready(function(){
 					nodes['book_item'+i].author=authors[i];
 					nodes['book_item'+i].category=categories[i];
 					nodes['book_item'+i].image=image_links[i];
-					//nodes['book_item'+i].shape='dot';
-					
-					
+					//nodes['book_item'+i].shape='dot';		
 				}
-				
-				// Insert data from nodes into b_data.nodes
 				Object.assign(b_data.nodes,genre);
-				Object.assign(b_data.nodes,nodes);
-				
-				
-				
-				// Draw b_data and its data into canvas
-				sys.graft(b_data);
-				
-				
-				
-				/*
-					console.log(titles);
-					console.log(authors);
-					console.log(categories);
-					console.log(image_links);
-				*/
-				console.log(b_data);
+				Object.assign(b_data.nodes,nodes);// Insert data from nodes into b_data.nodes
+				sys.graft(b_data);// Draw b_data and its data into canvas
+				console.log(b_data);// for testing
 				
 			}});
 	});
