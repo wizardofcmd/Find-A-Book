dd75d4013b129d062a4ba11129beaad902c02552
StephenMoran2000 <60134471+StephenMoran2000@users.noreply.github.com>
2020-12-12 14:11:40 +0000

    maybe

--- php/search.php
+++ php/search.php
@@ -2,7 +2,7 @@
 /*header('Set-Cookie: cross-site-cookie=name; SameSite=None; Secure');
 session_start();
 error_reporting(0);// With this no error reporting will be there*/
-require_ once("../api/config/database.php");
+require_once("../api/config/database.php");
 function get_book($conn , $term){
  $query = "SELECT * FROM usersreviews WHERE book LIKE '%".$term."%' ORDER BY book ASC";
  $result = mysqli_query($conn, $query);
