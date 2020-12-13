2c7c50713471ac5b4ffd40f40c10f5f996299a74
naisuuu <cjmosawan@yahoo.ie>
2020-12-13 23:54:42 +0000

    Update details.js

--- js/details.js
+++ js/details.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
   var jsarray = JSON.parse(sessionStorage.getItem("jsarray"))
   console.log(jsarray);
 
-  document.getElementById("h_label").innerHTML = jsarray[0];
-  document.getElementById("h_desc").innerHTML = jsarray[1];
-  document.getElementById("h_author").innerHTML = jsarray[2];
-  document.getElementById("h_genre").innerHTML = jsarray[3];
-  document.getElementById("h_isbn").innerHTML = jsarray[6];
-  document.getElementById("h_rating").innerHTML = jsarray[7];
+  document.getElementById("h_label").innerHTML = jsarray[0].slice();
+  document.getElementById("h_desc").innerHTML = jsarray[1].slice();
+  document.getElementById("h_author").innerHTML = jsarray[2].slice(2,-2);
+  document.getElementById("h_genre").innerHTML = jsarray[3].slice(2,-2);
+  document.getElementById("h_isbn").innerHTML = jsarray[6].slice(1,-1);
+  document.getElementById("h_rating").innerHTML = jsarray[7].slice(1,-1);
 
 
   var imagelink = jsarray[4].slice(1);
